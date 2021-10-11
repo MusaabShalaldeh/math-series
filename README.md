ed54bcb2a780fe5515c77a3605a358e6e7cbcea5
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-10-11 17:56:23 +0300

    test: adding first fibonacci test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Math Series
+
+### Open Branch
+
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,16 @@
 from math_series import __version__
+from math_series.series import fibonacci
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_fibonacci_1():
+    #Arrange
+    number = 10
+    expected = 50
+    #Act
+    result = fibonacci(10)
+    #Assert
+    assert result == expected
